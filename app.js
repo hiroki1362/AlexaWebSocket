3f7760bad0989ad2ebcbc9bfd486572d8bcb7da9
infodex-fujioka <hiroki.fujioka@infodex.co.jp>
2018-11-12 17:29:40 +0900

    Replay PORT.

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
-// const PORT = process.env.PORT || 7000;
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 app.get('/' , function(req, res){
     res.sendFile(__dirname+'/index.html');
